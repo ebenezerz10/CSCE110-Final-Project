51d17fbb9bccc381862e9d511822b9ac3c0d28af
noahwoinicki <63131233+noahwoinicki@users.noreply.github.com>
2020-12-05 18:24:18 -0600

    fix

--- project.py
+++ project.py
@@ -34,8 +34,6 @@ def menu():
     
     if choice == "1" or choice == "1.":
         read()
-        #Eb
-        #read csv file code
     elif choice == "2" or choice == "2.":
         s_report()
     elif choice == "3" or choice == "3.":
@@ -44,9 +42,8 @@ def menu():
         c_report()   
     elif choice == "5" or choice == "5.":
         c_charts()     
-    elif choice == "6" or choice == "6.":
+    elif choice == "6" or choice == "6." or choice == "q" or choice == "quit":
         sys.exit
-        #we might want to chose another option
     else: 
         print("You must select an option 1-6")
         print("Please try again")
@@ -82,20 +79,16 @@ def s_report():
         if student.isdigit() and len(student) == 10:
             print("This is a valid UIN") 
             break
+        #add to make sure it checks to see if the UIN is in the data
         else:
             print("This is not valid")
             student = input("Please enter the UIN of the student you would like to general a report for: ") 
-    if student in uin:
-        print('yes')
-    else:
-        print('no')
     txt = open(f"{student}.txt","w+")
     txt.write("Exams mean: {}\nLab mean: {}\nQuizzes mean: {}\nReading activites mean: {}\n".format(exam_m,lab_m,quiz_m,ra_m))
     #need to add score and letter grade
     txt.close
     main()
     
-
 def s_charts():
     pass
 
