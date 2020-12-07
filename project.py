952ba1a2910b6b3eaa715a8dece31c81aea94f2b
noahwoinicki <63131233+noahwoinicki@users.noreply.github.com>
2020-12-07 00:45:14 -0600

    added grading

--- project.py
+++ project.py
@@ -42,27 +42,25 @@ def menu():
     elif choice == "5" or choice == "5.":
         c_charts()   
     elif choice == "6" or choice == "6." or choice == "q" or choice == "quit":
-        sys.exit 
-        #we might change this to break
+        sys.exit
     else: 
         print("You must select an option 1-6")
         print("Please try again")
         menu()
         
 def read():
-    global student_report, uin, lab_m, quiz_m, ra_m, exam_m, project#, res, lab_f
+    global student_report, uin, lab_m, quiz_m, ra_m, exam_m, project, res, lab_f
     data = input("Please enter the path and name of your CSV file: ")
     #noah's path 
-    #C:\Users\noahw\OneDrive\Desktop\CSCE 110\Project\Data\grades.csv
+    #
     #eb's path 
     #C:\Users\ebenezerz10\Documents\GitHub\CSCE110-Final-Project
     with open (data) as f:
         data_read = csv.reader(f)  # reads file
         next(data_read, None)
         for row in data_read:
-            #Sprint(row)
             student_report = row[0:23]
-            #print(student_report)  
+            print(student_report)
             uin.append(row[0])
             lab_m.append(row[1:7])
             quiz_m.append(row[6:13])
@@ -70,20 +68,45 @@ def read():
             exam_m.append(row[18:22])
             project.append(row [22:23])
         menu()
-        return student_report, uin, lab_m, quiz_m, ra_m, exam_m, project#, res, lab_f
-        
+        return student_report, uin, lab_m, quiz_m, ra_m, exam_m, project        
 def s_report():
     while True:
-        #print("practice print of the student_report", student_report)
-        #print("practice print of uin", uin)
         student = input("Please enter the UIN of the student you would like to general a report for: ")
         if student in uin:
             print("This is a valid UIN")
             break
-        #add to make sure it checks to see if the UIN is in the data
         else: 
             print("This is not valid")
-        #txt.write("Exams mean: {}\nLab mean: {}\nQuizzes mean: {}\nReading activites mean: {}\n".format(exam_m),lab_f,quiz_m,ra_m))
+        for row in student_report:
+            uin_for_row = row[0]
+            total = 0
+            lab_total = 0
+            quiz_total = 0
+            reading_total = 0
+            exam_total = 0
+            project_total = 0 
+            for index in range(1,6):
+                thisGrade = float(row[index])
+                lab_total = lab_total + thisGrade
+                total = total + lab_total * .25
+            for index in range(7,12):
+                thisGrade = float(row[index])
+                quiz_total = quiz_total + thisGrade
+                total = total + quiz_total * .1
+            for index in range(13,19):
+                thisGrade = float(row[index])
+                reading_total = reading_total + thisGrade
+                total = total + reading_total * .1
+            for index in range(20,22):
+                thisGrade = float(row[index])
+                exam_total = exam_total + thisGrade
+                total = total + exam_total * .45
+            for index in range(20,22):
+                thisGrade = float(row[index])
+                project_total = project_total + thisGrade
+                total = total + project_total * .1	           
+        txt.write(total, lab_total, quiz_total, reading_total, exam_total, project_total)
+        print(total, lab_total, quiz_total, reading_total, exam_total, project_total)
         #need to add score and letter grade
     txt = open(f"{student}.txt","w+")
     print("Your report has been created as a .txt file")
